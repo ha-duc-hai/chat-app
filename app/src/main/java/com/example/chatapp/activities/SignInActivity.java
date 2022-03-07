4f2524ef77bfcb54f4301af422ba3bd408a7804c
Hai <tankinguyen2@gmail.com>
2022-03-07 08:33:59 +0700

    update sign in, sign up

--- app/src/main/java/com/example/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/example/chatapp/activities/SignInActivity.java
@@ -2,27 +2,15 @@ package com.example.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.chatapp.R;
-import com.example.chatapp.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
-
-    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivitySignInBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        setListeners();
+        setContentView(R.layout.activity_sign_in);
     }
-
-    private void setListeners() {
-        binding.textCreateNewAccount.setOnClickListener(v ->
-                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
-    }
-
 }
--- app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
+++ app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
@@ -5,17 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.chatapp.R;
-import com.example.chatapp.databinding.ActivitySignInBinding;
 import com.example.chatapp.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private ActivitySignUpBinding binding
+    private ActivitySignUpBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
     }
