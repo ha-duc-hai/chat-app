32f59369e917ff714bf7625296786d786d08f05c
duyhd0510 <duy.vd19010055@st.phenikaa-uni.edu.vn>
2022-03-07 15:05:34 +0700

    firebase settings

--- app/src/main/java/com/example/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/example/chatapp/activities/SignInActivity.java
@@ -2,15 +2,45 @@ package com.example.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
-
+    private ActivitySignInBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_sign_in);
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(
+                v->startActivity(new Intent(getApplicationContext(), SignUpActivity.class))
+        );
+        binding.buttonSignIn.setOnClickListener(v-> addDataToFirestore());
+    }
+
+    private void addDataToFirestore() {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "Chirag");
+        data.put("last_name", "Kachhadiva");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exeption -> {
+                    Toast.makeText(getApplicationContext(), exeption.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 }
